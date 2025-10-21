dd88ea972ef1e4667448e4696c2943a3779ae6ee
tsfmakko <racingf1@gmail.com>
2025-10-22 01:55:46 +0800

    feat: jsx格式化

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -1,13 +1,22 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 export default function Invoice() {
   return (
     <section className="max-w-lg mx-auto space-y-4">
       <h1 className="text-2xl font-semibold">電子發票</h1>
       <p>這裡是電子發票 /checkout/invoice 頁面。</p>
-        <NavLink to="/checkout/complete" className={({ isActive }) => `${isActive ? 'text-blue-600 font-medium' : 'text-gray-700 hover:text-blue-600'} py-[20px]`}>
-                            付款成功
-                        </NavLink> 
+      <NavLink
+        to="/checkout/complete"
+        className={({ isActive }) =>
+          `${
+            isActive
+              ? "text-blue-600 font-medium"
+              : "text-gray-700 hover:text-blue-600"
+          } py-[20px]`
+        }
+      >
+        付款成功
+      </NavLink>
     </section>
-  )
+  );
 }
