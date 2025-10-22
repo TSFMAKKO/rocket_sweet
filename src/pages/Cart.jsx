84c2be196f9b21d3ed783b9b64739fe5b8b326e0
tsfmakko <racingf1@gmail.com>
2025-10-22 10:28:29 +0800

    feat: 產生購物車需要的資料

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,25 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
+import { CART_ITEMS,SHIPPING_FEE } from "../data/cart";
+console.log("CART_ITEMS:", CART_ITEMS);
+console.log("SHIPPING_FEE:", SHIPPING_FEE);
+
 
 export default function Cart() {
   return (
     <section className="max-w-lg mx-auto space-y-4">
       <h1 className="text-2xl font-semibold">購物車</h1>
-      <NavLink to="/checkout/transport" className={({ isActive }) => `${isActive ? 'text-blue-600 font-medium' : 'text-gray-700 hover:text-blue-600'} py-[20px]`}>
+      <NavLink
+        to="/checkout/transport"
+        className={({ isActive }) =>
+          `${
+            isActive
+              ? "text-blue-600 font-medium"
+              : "text-gray-700 hover:text-blue-600"
+          } py-[20px]`
+        }
+      >
         運送
       </NavLink>
-
     </section>
-  )
+  );
 }
