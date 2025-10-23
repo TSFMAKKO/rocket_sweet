e0b5e65b7261da83d621cc4e1c0fbe1fb9e6d2f4
tsfmakko <racingf1@gmail.com>
2025-10-23 13:16:42 +0800

    feat: 完成首頁部分元件

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,13 +6,52 @@ export default function IndexPage() {
   return (
     //  max-sm:px-[20px]
     <section className="">
-      <h1 className="text-3xl font-bold tracking-tight">Index</h1>
-      <p className="text-slate-600">這是首頁</p>
-      <img
-        src={BASE_URL + "首頁1.avif"}
-        alt="甜點圖片"
-        className="w-full h-auto mb-[60px]"
-      />
+      {/* <h1 className="text-3xl font-bold tracking-tight">這是首頁</h1> */}
+      <div>
+        <img
+          src={BASE_URL + "首頁1.avif"}
+          alt="甜點圖片"
+          className="w-full h-auto"
+        />
+      </div>
+
+        {/* <!-- 本日精選 --> */}
+      <div class="flex w-[780px] mt-[-89px] mx-auto gap-x-[0.66px] h-64 text-center text-[24px] font-[300] relative overflow-hidden">
+        <div class="flex-1 flex justify-center items-center relative">
+          <img
+            src={`${BASE_URL}p-1.png`}
+            class="w-[260px] h-[256px] absolute inset-0 object-cover blur-[5px] opacity-80"
+          />
+          <div class="absolute inset-0 bg-[#FFE180]/80"></div>
+          <span class="relative [writing-mode:vertical-rl] [text-orientation:upright] inline-block leading-[34px] font-[600] text-green-800">
+            本日精選
+          </span>
+        </div>
+
+        {/* <!-- 人氣推薦 --> */}
+        <div class="flex-1 flex justify-center items-center relative">
+          <img
+            src={`${BASE_URL}p-3.png`}
+            class="w-[260px] h-[256px] absolute inset-0 object-cover blur-[5px] opacity-80"
+          />
+          <div class="absolute inset-0 bg-[#3F5D45]/80"></div>
+          <span class="relative [writing-mode:vertical-rl] [text-orientation:upright] inline-block leading-[34px] text-white">
+            人氣推薦
+          </span>
+        </div>
+
+        {/* <!-- 新品上市 --> */}
+        <div class="flex-1 flex justify-center items-center relative">
+          <img
+            src={`${BASE_URL}p-3.png`}
+            class="w-[260px] h-[256px] absolute inset-0 object-cover blur-[5px] opacity-80"
+          />
+          <div class="absolute inset-0 bg-[#3F5D45]/70"></div>
+          <span class="relative [writing-mode:vertical-rl] [text-orientation:upright] inline-block leading-[34px] text-white">
+            新品上市
+          </span>
+        </div>
+      </div>
     </section>
   );
 }
