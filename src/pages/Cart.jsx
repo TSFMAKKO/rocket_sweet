5547600780631941bed35f1fbcd0c9891a8d31c6
tsfmakko <racingf1@gmail.com>
2025-10-23 11:02:39 +0800

    完成cart的rwd

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -53,8 +53,8 @@ export default function Cart() {
       <div className="flex flex-col lg:flex-row gap-x-[20px]">
         {/* 左側：彈性區塊，手機/平板會撐滿寬度，桌機維持約 620px */}
         <div className="flex-1 min-w-0 lg:w-[620px]">
-          <div className="font-[600] text-[24px] flex justify-center items-center bg-[#EAF0ED] py-[16px] mb-[33px]">
-            購物車
+          <div className="max-sm:-ml-[42px] max-sm:w-[calc(100%+84px)] font-[600] text-[24px] flex justify-center items-center bg-[#EAF0ED] py-[16px] mb-[33px]">
+            您的購物車
           </div>
 
           {/* 商品列表 CART_ITEMS */}
@@ -66,11 +66,12 @@ export default function Cart() {
               qty: 2,
               image: BASE_URL + "photo-1514517220017-8ce97a34a7b6.avif",
             }, */}
+          {/* 桌機 */}
           <div>
             {items.map((item) => (
               <div
                 key={item.id}
-                className="flex justify-between py-2 border-b border-gray-200"
+                className="max-sm:hidden flex justify-between py-2 border-b border-gray-200"
               >
                 <div className="">
                   <img
@@ -109,10 +110,62 @@ export default function Cart() {
                 </div>
 
                 <div
-                  className="flex flex-col justify-center"
+                  className="max-sm:hidden flex flex-col justify-center"
                   onClick={() => delHandler(item.id)}
                 >
                   刪除
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* 手機 */}
+          <div>
+            {items.map((item) => (
+              <div
+                key={item.id}
+                className="sm:hidden flex relative mb-[60px] justify-between py-2 border-b border-gray-200"
+              >
+                <div className="flex-1">
+                  <img
+                    className="object-cover w-full h-[110px]"
+                    src={item?.image}
+                    alt={item?.name}
+                  />
+                </div>
+
+                <div className="flex-1 pl-[16px]">
+                  <div className="flex flex-col justify-center">
+                    <p>{item.name}</p>
+                    <p>
+                      {item?.currency} {item?.unitPrice.toLocaleString()}
+                    </p>
+                  </div>
+
+                  <div className="flex justify-center items-center">
+                    <button
+                      className="block px-3 py-2 border border-[#3F5D45]"
+                      onClick={() => changeQty(item.id, -1)}
+                      aria-label={`減少 ${item.name}`}
+                    >
+                      −
+                    </button>
+                    <div className="block px-4">{item.qty}</div>
+                    <button
+                      className="block px-3 py-2 border border-[#3F5D45]"
+                      onClick={() => changeQty(item.id, 1)}
+                      aria-label={`增加 ${item.name}`}
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+
+                {/* 價格 */}
+                <div className="absolute top-[100%]  border-b border-[#EAF0ED] text-[20px] flex justify-end font-[600] w-full ">
+                  <div className="flex justify-end items-center h-[60px]">
+                    {item?.currency} {getLineTotal(item).toLocaleString()}
+                  </div>
                 </div>
               </div>
             ))}
@@ -121,6 +174,7 @@ export default function Cart() {
 
         {/* 右側：使用可重用的 OrderSummary 元件（props: summary） */}
         <OrderSummary summary={summary} />
+        <OrderSummaryMobile summary={summary} />
       </div>
     </section>
   );
@@ -130,7 +184,7 @@ export default function Cart() {
 function OrderSummary({ summary }) {
   return (
     // { summary.subtotal.toLocaleString() && <p>123<p/>}
-    <aside className="w-full h-[300px] max-sm:h-auto lg:w-[300px] shrink-0 bg-[#3F5D45] text-[#EAF0ED] p-4">
+    <aside className="max-sm:hidden w-full h-[300px] max-sm:h-auto lg:w-[300px] shrink-0 bg-[#3F5D45] text-[#EAF0ED] p-4">
       {/* 標題 */}
       <h3 className="text-[24px] font-[600] text-center py-[16px] border-b border-[#EAF0ED]">
         訂單摘要
@@ -169,5 +223,54 @@ function OrderSummary({ summary }) {
         </NavLink>
       </div>
     </aside>
+
+    
+  );
+}
+
+function OrderSummaryMobile({ summary }) {
+  return (
+    // { summary.subtotal.toLocaleString() && <p>123<p/>}
+    <aside className="sm:hidden w-full h-[300px] max-sm:h-auto lg:w-[300px] shrink-0 text-[#3F5D45] p-[16px]">
+      {/* 標題 */}
+      <h3 className="text-[24px] font-[600] text-center py-[16px] border-b bg-[#EAF0ED] text-[#3F5D45] border-[#EAF0ED] ml-[-16px] w-[calc(100%+32px)] mt-[-16px]">
+        訂單摘要
+      </h3>
+
+      {/* 金額明細 */}
+
+      <div className="text-sm flex flex-col gap-y-[8px] py-[16px] font-[300] text-[16px]">
+        <div className="flex justify-between">
+          <span>小計</span>
+          <span>
+            {summary.currency} {summary.subtotal.toLocaleString()}
+          </span>
+        </div>
+        <div className="flex justify-between">
+          <span>運費</span>
+          <span>
+            {summary.currency} {summary.shipping.toLocaleString()}
+          </span>
+        </div>
+        <div className="flex justify-between font-semibold pt-2">
+          <span>總計</span>
+          <span>
+            {summary.currency} {summary.total.toLocaleString()}
+          </span>
+        </div>
+      </div>
+
+      {/* 結帳按鈕（用 NavLink 前往下一步） */}
+      <div className=" font-[600] text-[24px] ml-[calc(-58px)] w-[calc(100%+116px)] mb-[-16px]">
+        <NavLink
+          to="/checkout/transport"
+          className="block text-center bg-[#FFE180] py-[16px] text-[#3F5D45] font-medium"
+        >
+          結帳
+        </NavLink>
+      </div>
+    </aside>
+
+    
   );
 }
