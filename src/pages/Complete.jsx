4961ca648313418683472d9357bd03567e703f22
tsfmakko <racingf1@gmail.com>
2025-10-23 10:19:50 +0800

    完成購物車的付款成功rwd

--- src/pages/Complete.jsx
+++ src/pages/Complete.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom'
 
 export default function Complete() {
   return (
-    <section className="w-full h-[460px] bg-[url('/photo-1505935428862-770b6f24f629.avif')] bg-no-repeat bg-cover bg-center flex items-center">
+    <section className="w-full h-[460px] max-sm:h-[518px] max-sm:relative max-sm:mb-[40px] bg-[url('/photo-1505935428862-770b6f24f629.avif')] bg-no-repeat bg-cover bg-center flex items-center">
       <div className="max-w-lg mx-auto flex flex-col items-center text-[#3F5D45]">
         <div className="flex justify-center items-center mb-[32px]">
           <span className="w-[20px] h-[20px] bg-[#3F5D45] rounded-full flex justify-center items-center">
@@ -26,7 +26,7 @@ export default function Complete() {
         <h1 className="text-[44px] tracking-[4px] font-[600] mb-[50px]">付款成功</h1>
         <NavLink
           to="/product"
-          className="inline-block text-[24px] py-[16px] px-[100px] text-center bg-[#FFE180] text-[#3F5D45] font-medium"
+          className="inline-block max-sm:absolute max-sm:top-[100%] max-sm:w-full text-[24px] py-[16px] px-[100px] text-center bg-[#FFE180] text-[#3F5D45] font-medium"
         >
           繼續逛逛
         </NavLink>
