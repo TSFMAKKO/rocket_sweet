b15c2c949f70510e765bf9f5123ff3fd2320082f
tsfmakko <racingf1@gmail.com>
2025-10-27 16:16:50 +0800

    fix: 修正駝峰命名

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -24,13 +24,13 @@ export default function Payment() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="#EAF0ED"
                   className="w-4 h-4"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M5 13l4 4L19 7"
                   />
                 </svg>
